c8a6ae928dc1fabe93ed7bb2d2fee652660c8724
Tobias <tobias.borjesson@hotmail.se>
2013-03-28 18:15:53 +0100

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,7 @@ require_once('constants.php');
 require_once('model.php');
 require_once('controller.php');
 require_once('view.php');
-//require_once(prettyview.php);
+
 
 
 \model\startup();
@@ -30,14 +30,6 @@ if(!\model\playerAlive())
  \view\outputObituary();
 }
 
-/*
-entry point
-\model\startup();
-if(\controller\checkForInput())
-{
-	$eventText = \model\doEvent();
-}
-\view\outputView($eventText);
-*/
+
 
 ?>
